96880f5c845ddf24c50b58691924bd9b94dffbd1
JordanEngland <lcplengland@gmail.com>
2023-06-09 17:07:20 -0500

    updated readme and  index.js

--- README1.md
+++ README1.md
@@ -3,7 +3,9 @@
 
 ## Description
 
-will provide later.....
+In this SQL project, I built a powerful database management system using MySQL and Node.js. With just a few commands, I created an application that handles employee management tasks effortlessly. Using the command line, I can easily view and organize departments, roles, and employees. I can add new entries, update employee roles, and even delete unnecessary data. The application interacts with the MySQL database behind the scenes, executing SQL queries to retrieve, insert, update, and delete data. With a user-friendly interface and clear prompts, the application streamlines the process of managing employee information. It's a great tool for professionals like me who need to handle employee data efficiently.
+
+<img src="assets\tracker.gif">
 
 ## Table of Contents
 
@@ -13,7 +15,9 @@ will provide later.....
 
 ## Installation
 
+Link to github repo-https://github.com/JordanEngland/challenge-12
 
+To Watch video of app in use click here->
 
 ## Usage
 
--- index.js
+++ index.js
@@ -356,3 +356,184 @@ function updateEmployeeRole() {
     });
   });
 }
+// Function to delete an employee
+function deleteEmployee() {
+  const query = 'SELECT * FROM employee';
+  connection.query(query, (err, employees) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt({
+        name: 'employee',
+        type: 'list',
+        message: "Select the employee you want to delete:",
+        choices: employees.map((employee) => ({
+          name: `${employee.first_name} ${employee.last_name}`,
+          value: employee.id,
+        })),
+      })
+      .then((answer) => {
+        const query = 'DELETE FROM employee WHERE id = ?';
+        connection.query(query, [answer.employee], (err, res) => {
+          if (err) throw err;
+          console.log(`\nEmployee deleted successfully.`);
+          startEmployeeTracker();
+        });
+      });
+  });
+}
+/// Function to delete a department
+function deleteDepartment() {
+  const query = 'SELECT * FROM department';
+  connection.query(query, (err, departments) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: 'department',
+          type: 'list',
+          message: 'Select the department to delete:',
+          choices: departments.map((department) => ({
+            name: department.name,
+            value: department.id,
+          })),
+        },
+      ])
+      .then((answer) => {
+            connection.query(
+          'DELETE FROM role WHERE department_id = ?',
+          [answer.department],
+          (err, res) => {
+            if (err) throw err;
+            console.log('Roles associated with the department have been deleted.');
+              connection.query(
+              'DELETE FROM department WHERE id = ?',
+              [answer.department],
+              (err, res) => {
+                if (err) throw err;
+                console.log('Department deleted successfully.');
+                startEmployeeTracker();
+              }
+            );
+          }
+        );
+      });
+  });
+}
+// Function to delete a role
+function deleteRole() {
+  const query = 'SELECT * FROM role';
+  connection.query(query, (err, roles) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt({
+        name: 'role',
+        type: 'list',
+        message: "Select the role you want to delete:",
+        choices: roles.map((role) => ({
+          name: role.title,
+          value: role.id,
+        })),
+      })
+      .then((answer) => {
+        const query = 'DELETE FROM role WHERE id = ?';
+        connection.query(query, [answer.role], (err, res) => {
+          if (err) throw err;
+          console.log(`\nRole deleted successfully.`);
+          startEmployeeTracker();
+        });
+      });
+  });
+}
+// Function to update an employee's manager
+function updateEmployeeManager() {
+  const query = 'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee';
+  connection.query(query, (err, employees) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: 'employee',
+          type: 'list',
+          message: 'Select the employee to update:',
+          choices: employees.map((employee) => ({
+            name: employee.name,
+            value: employee.id,
+          })),
+        },
+        {
+          name: 'manager',
+          type: 'list',
+          message: "Select the employee's new manager:",
+          choices: [
+            { name: 'None', value: null },
+            ...employees.map((employee) => ({
+              name: employee.name,
+              value: employee.id,
+            })),
+          ],
+        },
+      ])
+      .then((answer) => {
+        const query = 'UPDATE employee SET manager_id = ? WHERE id = ?';
+        connection.query(query, [answer.manager, answer.employee], (err, res) => {
+          if (err) throw err;
+          console.log('Employee manager updated successfully.');
+          startEmployeeTracker();
+        });
+      });
+  });
+}
+
+// Function to view employees by manager
+function viewEmployeesByManager() {
+  const query = `
+    SELECT
+      m.id AS manager_id,
+      CONCAT(m.first_name, ' ', m.last_name) AS manager_name,
+      e.id,
+      CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+      role.title,
+      department.name AS department,
+      role.salary
+    FROM employee e
+    INNER JOIN role ON e.role_id = role.id
+    INNER JOIN department ON role.department_id = department.id
+    LEFT JOIN employee m ON e.manager_id = m.id
+    ORDER BY manager_name, employee_name
+  `;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.log('\n');
+    console.table(res);
+    startEmployeeTracker();
+  });
+}
+
+// Function to view employees by department
+function viewEmployeesByDepartment() {
+  const query = `
+    SELECT
+      department.id AS department_id,
+      department.name AS department,
+      e.id,
+      CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+      role.title,
+      role.salary,
+      CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    INNER JOIN role ON e.role_id = role.id
+    INNER JOIN department ON role.department_id = department.id
+    LEFT JOIN employee m ON e.manager_id = m.id
+    ORDER BY department
+  `;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.log('\n');
+    console.table(res);
+    startEmployeeTracker();
+  });
+}
